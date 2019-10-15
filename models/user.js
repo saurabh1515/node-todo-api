79b72ad0d9e1cd5b58b489335083221bfe051140
Saurabh Shukla <saurabh.shukla244@gmail.com>
2019-10-15 15:02:58 +0530

    Added authentication

--- models/user.js
+++ models/user.js
@@ -1,12 +1,73 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
         trim: true,
-    }
+        unique: true,
+        validate: {
+            validator:  validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObj = user.toObject();
+
+    return _.pick(userObj, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, 'abc123');
+    } catch(e) {
+        return Promise.reject();
+    }
+    
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ mongoose.promise = global.promise;
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -95,6 +96,25 @@ app.patch('/todos/:id', (req, res) => {
     }).catch( (e) => {
         res.status(400).send();
     });
+});
+
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then( () => {
+        return user.generateAuthToken();
+    }).then( (token) => {
+        res.header('x-auth', token).send(user);
+    }).catch( (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
